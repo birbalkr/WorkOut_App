ad67f35d61a91d2d610c5341f8b6430e9b0b1cfe
birbalkr <birbalkr1435@gmail.com>
2024-04-22 22:21:42 +0530

    Start App

---
+++ MY_WorkOut_App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MY_WorkOut_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
